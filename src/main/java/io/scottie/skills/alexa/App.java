c24cbaf06a7855b3c752db40e605cf8fb2648cdb
Scott Enriquez <scottie@enriquez.io>
2016-12-03 21:41:04 -0600

    Adding initial structure

---
+++ src/main/java/io/scottie/skills/alexa/App.java
@@ -0,0 +1,13 @@
+package io.scottie.skills.alexa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
